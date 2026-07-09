6fd381206f16dcc6c3690ad5350cf5604eb9fbc4
Richard Meneses <Ing.Sistemas.Meneses@hotmail.com>
2026-07-09 16:26:20 -0500

    creando la clase Producto, metodos

---
+++ sistema_inventario.py
@@ -0,0 +1,16 @@
+class Producto:
+    #constructor 
+    def __init__(self, nombre: str,precio : float, cantidad : int):
+        self.nombre =  nombre
+        self.precio = precio
+        self.cantidad = cantidad
+    
+    #metodos
+    def actualizar_precio(self, nuevo_precio):
+        self.nuevo_precio = nuevo_precio
+        if self.nuevo_precio = 0:
+
+        
+   
+if __name__ == "__main__":
+    pass
