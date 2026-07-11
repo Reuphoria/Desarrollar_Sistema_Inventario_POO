b75ec460978e3b2d2a91c72984fb7ce4e54aa16e
Richard Meneses <Ing.Sistemas.Meneses@hotmail.com>
2026-07-11 12:42:28 -0500

    Terminar class inventario, creando el menu

--- sistema_inventario.py
+++ sistema_inventario.py
@@ -7,10 +7,9 @@ class Producto:
     
     #metodos actualizar_precio(nuevo_precio): para modificar el precio validando que sea mayor o igual que cero
     def actualizar_precio(self, nuevo_precio):
-        if self.nuevo_precio >= 0:
+        if nuevo_precio >= 0:
             self.precio = nuevo_precio
 
-    
     #metodo actualizar_cantidad(nueva_cantidad): para modificar la cantidad validando que sea mayor o igual a cero
     def actualizar_cantidad(self, nueva_cantidad):
         self.cantidad = nueva_cantidad
@@ -25,14 +24,39 @@ class Producto:
         
 class Inventario:
     #constructor para crear una lista
-    def __int__(self):
+    def __init__(self):
         self.almacenar_productos = []
     # añadir un produto a mi lista de producto
     def agregar_producto(self, producto):
         self.almacenar_productos.append(producto)
-    
+    # buscar un producto en el almacen
     def buscar_producto(self, nombre):
-        pass
+        for producto in self.almacenar_productos:
+            if nombre.lower() in producto.nombre.lower():
+                return producto
+        return None
+    #sumar el valor del inventario
+    def calcular_valor_inventario(self):
+        Total_productos = 0
+        for producto in self.almacenar_productos:
+            Total_producto += producto.calcular_valor_total()
+        return Total_productos
+    # mostrar todos los productos del inventario
+    def listar_productos(self):
+        for producto in self.almacenar_productos:
+            print(producto)
 
+def menu_principal():
+    try:
+        opcion = int(input("""Menu\n
+                           
+        1. Agregar Producto\n
+        2. Buscar Producto\n
+        3. Listar Productos\n
+        4. Calcular Valor\n
+        5. Exit                      
+        """))
+    except:
+        pass
 if __name__ == "__main__":
     pass
